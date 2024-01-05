2e2e07393786051637f5d1b730f905f57ec30c9c
Vlad <wladimir.pushkutse@gmx.de>
2024-01-05 13:48:06 +0100

    Trainingrecordview + Trainigview + Userview fertig

--- fitnessTrackerApp.py
+++ fitnessTrackerApp.py
@@ -26,14 +26,17 @@ class FitnessTrackerApp(tk.Tk):
         self.Startview = Startview(parent=self.container, controller=self)
         self.Userview = Userview(parent=self.container, controller=self)
         self.Trainingview = Trainingview(parent=self.container, controller=self)
+        self.Trainingrecordview = Trainingrecordview(parent=self.container, controller=self)
 
         self.frames['sv'] = self.Startview
         self.frames['uv'] = self.Userview
         self.frames['tv'] = self.Trainingview
+        self.frames['trv'] = self.Trainingrecordview
 
         self.Startview.grid(row=0, column=0, sticky="nsew")
         self.Userview.grid(row=0, column=0, sticky="nsew")
         self.Trainingview.grid(row=0, column=0, sticky="nsew")
+        self.Trainingrecordview.grid(row=0, column=0, sticky="nsew")
 
         self.show_frame("sv")
         # SQLite-Datenbankverbindung herstellen
@@ -51,9 +54,49 @@ class FitnessTrackerApp(tk.Tk):
         frame.show()
         frame.tkraise()
 
+
+    def record_user(self):
+
+        name = self.Userview.name_entry.get()
+        age = self.Userview.weight_entry.get()
+        weight = self.Userview.weight_entry.get()
+        fl = self.Userview.fl_entry.get()
+
+        connection = self.db.get_connection()
+        cursor = connection.cursor()
+
+        print(weight)
+        # Überprüfen, ob die Eingabe eine positive Zahl ist
+        try:
+            weight = float(weight)
+            if weight <= 0:
+                raise ValueError("Das Gewicht muss eine positive Zahl sein.")
+        except ValueError:
+            self.Startview.message_Label.configure(text="Bitte geben Sie eine gültige Gewichtsangabe ein.",
+                                                   foreground="red")
+
+            return
+
+
+        # Gewichtsverlauf in die Datenbank einfügen
+        cursor.execute("INSERT INTO user (name, age, weight, fl) VALUES (?, ?, ?, ? )", (name, age, weight, fl ))
+
+
+        # Meldung anzeigen, dass das Gewicht erfolgreich aufgezeichnet wurde
+        self.Userview.message_Label.configure(text=f"Hallo {name}, es kann losgehen",
+                                               foreground="green")
+
+        # Eingabefelder leeren
+        self.Startview.weight_entry.delete(0, tki.END)
+
+        cursor.execute("SELECT * FROM user")
+        users = cursor.fetchall()
+
+        print(users)
+
     def record_workout(self):
         # Eingabewerte vom Benutzer abrufen
-        activity = self.Startview.workout_entry.get()
+        activity = self.Trainingrecordview.workout_entry.get()
         connection = self.db.get_connection()
         cursor = connection.cursor()
 
@@ -62,7 +105,7 @@ class FitnessTrackerApp(tk.Tk):
         # Überprüfen, ob die Eingabe nicht leer ist
         if activity == "":
             # Zeige eine Meldung an, dass das Feld nicht leer sein darf
-            self.Startview.message_Label.configure(text='Bitte Training eingeben', foreground='red')
+            self.Trainingrecordview.message_Label.configure(text='Bitte Training eingeben', foreground='red')
             return
 
         # Aktuelles Datum und Uhrzeit abrufen
@@ -73,11 +116,11 @@ class FitnessTrackerApp(tk.Tk):
         cursor.execute("INSERT INTO workouts (activity, date) VALUES (?, ?)", (activity, current_date))
 
         # Meldung anzeigen, dass die Trainingsaktivität erfolgreich aufgezeichnet wurde
-        self.Startview.message_Label.configure(text=f"Trainingsaktivität '{activity}' erfolgreich aufgezeichnet.",
+        self.Trainingrecordview.message_Label.configure(text=f"Trainingsaktivität '{activity}' erfolgreich aufgezeichnet.",
                                            foreground="green")
 
         # Eingabefelder leeren
-        self.Startview.workout_entry.delete(0, tki.END)
+        self.Trainingrecordview.workout_entry.delete(0, tki.END)
 
         cursor.execute("SELECT activity, date FROM workouts ORDER BY date DESC")
         workouts = cursor.fetchall()
@@ -245,15 +288,21 @@ class FitnessTrackerApp(tk.Tk):
             tki.Label(weight_logs_frame, text=log[0]).grid(row=index, column=0, padx=5, pady=5)
             tki.Label(weight_logs_frame, text=log[1]).grid(row=index, column=1, padx=5, pady=5)
 
+
+    #Hier werden alle Funktionen allen Buttons aller Views zugeordnet
     def map_button_functions(self):
         self.Startview.show_workout_button.configure(command=lambda: self.show_frame("tv"))
         # self.Startview.show_workout_button.configure(command=self.show_workouts)
         self.Startview.show_meal_button.configure(command=self.show_meals)
         self.Startview.show_weight_button.configure(command=self.show_weight_logs)
-
-        self.Startview.record_workout_button.configure(command=self.record_workout)
+        # self.Startview.record_workout_button.configure(command=self.record_workout)
+        self.Startview.record_workout_button.configure(command=lambda: self.show_frame("trv"))
         self.Startview.record_meal_button.configure(command=self.record_meal)
         self.Startview.record_weight_button.configure(command=self.record_weight)
+
+        self.Userview.create_user_button.configure(command=self.record_user)
+
+        self.Trainingrecordview.safe_workout_button.configure(command=self.record_workout)
 
 
 if __name__ == "__main__":
--- views.py
+++ views.py
@@ -8,8 +8,10 @@ class Startview(tk.Frame):
         tk.Frame.__init__(self, parent)
         self.controller = controller
 
+        self.db = Database()
+        self.connection = self.db.get_connection()
+        self.cursor = self.connection.cursor()
 
-        self.workout_entry = tk.Entry(self)
         self.weight_entry = tk.Entry(self)
         self.meal_entry = tk.Entry(self)
 
@@ -17,30 +19,37 @@ class Startview(tk.Frame):
         self.create_user_button = tk.Button(self, text="User erstellen", command=lambda: controller.show_frame("uv"))
 
         # Buttons um Werte einzutragen und zu speichern
-        self.record_workout_button = tk.Button(self, text="Trainingsaktivität aufzeichnen")
         self.record_meal_button = tk.Button(self, text="Mahlzeit aufzeichnen")
         self.record_weight_button = tk.Button(self, text="Gewicht aufzeichnen")
 
         # Buttons um Werte anzuzeigen
+        self.record_workout_button = tk.Button(self, text="Trainingsaktivität aufzeichnen")
         self.show_workout_button = tk.Button(self, text="Trainingsaktivitäten anzeigen")
         self.show_meal_button = tk.Button(self, text="Mahlzeiten anzeigen")
         self.show_weight_button = tk.Button(self, text="Gewichtsverlauf anzeigen")
 
         # Labels der Erstansicht
-        self.training_label = tk.Label(self, text="Trainingsaktivität:")
+
+        self.name_label = tk.Label(self)
         self.meal_Label = tk.Label(self, text="Mahlzeit:")
         self.weight_label = tk.Label(self, text="Gewicht (kg):")
-
         self.message_Label = tk.Label(self, text="lalala")
 
 
 
     def show(self):
-        self.create_user_button.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
 
-        # Label für Trainingsaktivitäten
-        self.training_label.grid(row=1, column=0, padx=10, pady=10)
-        self.workout_entry.grid(row=1, column=1, padx=10, pady=10)
+        self.cursor.execute("SELECT name FROM user WHERE ID = 1 ")
+        name = self.cursor.fetchone()
+        print('Name: ' + str(name))
+
+        if not name:
+            self.create_user_button.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
+        else:
+            self.create_user_button.grid_forget()
+            self.name_label.configure(text=name)
+            self.name_label.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
+
 
         # Label für Mahlzeiten
         self.meal_Label.grid(row=2, column=0, padx=10, pady=10)
@@ -51,7 +60,6 @@ class Startview(tk.Frame):
         self.weight_entry.grid(row=3, column=1, padx=10, pady=10)
 
         # Buttons zum Recorden platzieren
-        self.record_workout_button.grid(row=4, column=0, columnspan=2, padx=10, pady=10)
         self.record_meal_button.grid(row=5, column=0, columnspan=2, padx=10, pady=10)
         self.record_weight_button.grid(row=6, column=0, columnspan=2, padx=10, pady=10)
 
@@ -61,6 +69,7 @@ class Startview(tk.Frame):
         self.show_weight_button.grid(row=9, column=0, columnspan=2, padx=10, pady=10)
 
         self.message_Label.grid(row=10, column=0, columnspan=2, pady=10)
+        self.record_workout_button.grid(row=4, column=0, columnspan=2, padx=10, pady=10)
 
 
 class Userview(tk.Frame):
@@ -68,26 +77,28 @@ class Userview(tk.Frame):
         tk.Frame.__init__(self, parent)
         self.controller = controller
 
-        self.workout_entry = tk.Entry(self)
+        self.name_entry = tk.Entry(self)
+        self.age_entry = tk.Entry(self)
         self.weight_entry = tk.Entry(self)
-        self.meal_entry = tk.Entry(self)
         self.fl_entry = tk.Entry(self)
 
         # Labels der Erstansicht
-        self.name_label = tk.Label(self, text="Trainingsaktivität:")
-        self.age_Label = tk.Label(self, text="Mahlzeit:")
+        self.name_label = tk.Label(self, text="Name:")
+        self.age_Label = tk.Label(self, text="Alter:")
         self.weight_label = tk.Label(self, text="Gewicht (kg):")
-        self.fl_label = tk.Label(self, text="Gewicht (kg):")
+        self.fl_label = tk.Label(self, text="Fitnesslevel:")
+        self.create_user_button = tk.Button(self, text="User speichern")
 
+        self.message_Label = tk.Label(self, text="lalala")
 
 
     def show(self):
         self.name_label.grid(row=1, column=0, padx=10, pady=10)
-        self.workout_entry.grid(row=1, column=1, padx=10, pady=10)
+        self.name_entry.grid(row=1, column=1, padx=10, pady=10)
 
         # Label für Mahlzeiten
         self.age_Label.grid(row=2, column=0, padx=10, pady=10)
-        self.meal_entry.grid(row=2, column=1, padx=10, pady=10)
+        self.age_entry.grid(row=2, column=1, padx=10, pady=10)
 
         # Label für Gewichtsverlauf
         self.weight_label.grid(row=3, column=0, padx=10, pady=10)
@@ -101,11 +112,10 @@ class Userview(tk.Frame):
                            command=lambda: self.controller.show_frame("sv"))
         button.grid(row=11, column=0, columnspan=1, padx=10, pady=10)
 
-        # Button um User zu erstellen
-        self.create_user_button = tk.Button(self, text="User speichern")
-
         # Buttons zum Aufzeichnen von Daten
         self.create_user_button.grid(row=11, column=1, columnspan=2, padx=10, pady=10)
+
+        self.message_Label.grid(row=10, column=0, columnspan=2, pady=10)
 
 
 
@@ -119,6 +129,7 @@ class Trainingview(tk.Frame):
 
         self.activity_label=tk.Label(self, text="Aktivität")
         self.date_label = tk.Label(self, text="Datum")
+
 
     def show(self):
         # Ein Label für die Spaltenüberschriften
@@ -137,6 +148,31 @@ class Trainingview(tk.Frame):
             tk.Label(self, text=workout[0]).grid(row=index, column=0, padx=5, pady=5)
             tk.Label(self, text=workout[1]).grid(row=index, column=1, padx=5, pady=5)
 
+
+
+class Trainingrecordview(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        self.db = Database()
+        self.connection = self.db.get_connection()
+        self.cursor = self.connection.cursor()
+
+        self.training_label = tk.Label(self, text="Trainingsaktivität:")
+        self.workout_entry = tk.Entry(self)
+        self.message_Label = tk.Label(self, text="lalala")
+        self.safe_workout_button = tk.Button(self, text="Trainingsaktivität aufzeichnen")
+
+    def show(self):
+        self.training_label.grid(row=1, column=0, padx=10, pady=10)
+        self.workout_entry.grid(row=1, column=1, padx=10, pady=10)
+        self.safe_workout_button.grid(row=4, column=0, columnspan=2, padx=10, pady=10)
+        self.message_Label.grid(row=10, column=0, columnspan=2, pady=10)
+
+        button = tk.Button(self, text="Go to the start page",
+                           command=lambda: self.controller.show_frame("sv"))
+        button.grid(row=11, column=0, columnspan=1, padx=10, pady=10)
 
 
 class Mealview(tk.Frame):
